600443b97084564e3c5a362c13380aa64e6181b3
mustopha <mustophaqadir@gmail.com>
2022-09-09 20:13:04 -0700

    tenth task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all combinations of single-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+{
+putchar(x);
+if (!(x == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
