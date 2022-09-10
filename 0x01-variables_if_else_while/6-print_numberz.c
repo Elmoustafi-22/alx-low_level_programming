39b48da1760a9733568cfd39ffc112830892489b
mustopha <mustophaqadir@gmail.com>
2022-09-09 19:42:43 -0700

    seventh task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print single digits
+ * Return: last digit
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
