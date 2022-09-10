fa268ba37e14a6e31eafecd562b2eb9bf80cf91a
mustopha <mustophaqadir@gmail.com>
2022-09-09 20:00:27 -0700

    ninth tasks

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the base 16 numbers in lowercase
+ * followed by a line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+char c;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+for (c = 'a'; c <= 'f' c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
