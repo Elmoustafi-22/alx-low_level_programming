c25ce0fe1348f742a4beccb8fc7499a23e146e22
mustopha <mustophaqadir@gmail.com>
2022-09-09 18:49:22 -0700

    third task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Lower case letters
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
