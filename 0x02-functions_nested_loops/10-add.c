0132e34d26abe5e5749d65cd91c994eaf4951432
mustopha <mustophaqadir@gmail.com>
2022-09-13 20:10:51 -0700

    eleventh tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - a function that adds two integers and returns the result.
+ * @a: first argument
+ * @b: second argument
+ * Return: sum
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
