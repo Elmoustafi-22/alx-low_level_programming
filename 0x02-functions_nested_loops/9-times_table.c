807ea334f7923f8525f3855e322ad6072c5599da
mustopha <mustophaqadir@gmail.com>
2022-09-13 19:54:59 -0700

    tenth tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,13 +12,11 @@ for (a = 0; a < 10; a++)
 {
 for (b = 0; b < 10; b++)
 {
- c = b * a;
-
- if (b == 0)
+c = b * a;
+if (b == 0)
 {
 _putchar(c + '0');
 }
-
 if (c < 10 && b != 0)
 {
 _putchar(',');
@@ -26,7 +24,6 @@ _putchar(' ');
 _putchar(' ');
 _putchar(c + '0');
 }
-
 else if (c >= 10)
 {
 _putchar(',');
