7c6c3461c9f28973dca0246a2a52643c6fc39672
mustopha <mustophaqadir@gmail.com>
2022-09-13 17:37:36 -0700

    second task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - function to print a-z 
  * Return: Always 0
  */
 void print_alphabet(void)
 {
 char c;
-for(c = 'a'; c <= 'z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 
