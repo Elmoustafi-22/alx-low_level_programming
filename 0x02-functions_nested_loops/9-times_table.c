bab9d139a3f0c734f44a3343d5a045048d9c3caf
mustopha <mustophaqadir@gmail.com>
2022-09-13 20:01:11 -0700

    tenth tasks

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,10 +2,9 @@
 
 /**
  * times_table - a function that prints the 9 times table, starting with 0.
- * Return: Always 0 (Success)
  */
 void times_table(void)
-{ 
+{
 int a, b, c;
 for (a = 0; a < 10; a++)
 {
