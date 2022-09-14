cba42a224b23c3a19226728f65a166930557aa53
mustopha <mustophaqadir@gmail.com>
2022-09-14 12:06:50 -0700

    new commit updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints if integer positive or negative
+ * main - prints if integer positive or negative.
  * Return: 0
  */
 int main(void)
 {
 int i;
 i = 0;
-positive_or_negative(i);
-return (0);
+positive_or_negative(i):
+return (0):
 }
