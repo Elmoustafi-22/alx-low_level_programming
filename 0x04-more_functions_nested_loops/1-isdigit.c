e3cd9ff22fc63fca69d4e8dd665ab0f7c58c3232
mustopha <mustophaqadir@gmail.com>
2022-09-15 07:47:31 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - check the code for Holberton School students.
- * @c: characters
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: an argument
  * Return: 1 and 0
  */
 int _isdigit(int c)
 {
-if (c >= 0 && c <= 9)
+if (c >= '0' && c <= '9')
 return (1);
 return (0);
 }
