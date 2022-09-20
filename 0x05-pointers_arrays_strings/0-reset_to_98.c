8f818208518c8fa3cdc16049b811eced20d1cc8e
mustopha <mustophaqadir@gmail.com>
2022-09-19 17:37:12 -0700

    First Task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * void reset_to_98 - a functions that takes a pointer to an int,
+ * as parameters and updates it to 98
+ * @*n: takes the parameter
+ * Return: success
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
