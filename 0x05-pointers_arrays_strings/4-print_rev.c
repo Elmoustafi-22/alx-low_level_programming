69ad39a5f0fd2a09de0f0549e8d1040ff4961965
mustopha <mustophaqadir@gmail.com>
2022-09-19 18:36:00 -0700

    Fifth task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (--i >= 0)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
