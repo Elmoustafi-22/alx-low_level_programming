b53e0e688e71133d9cd9b821745c85d8253149f3
mustopha <mustophaqadir@gmail.com>
2022-09-27 05:00:24 -0700

    First task (0-memset.c)

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,9 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int index;
 
-	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
-	return (0);
+	for (index = 0; index < n; index++)
+		s[index] = b;
+	return (s);
 }
