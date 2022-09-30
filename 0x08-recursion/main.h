cd7065871cca6abfba34b7c00675bc8e55e6b4dd
mustopha <mustophaqadir@gmail.com>
2022-09-29 20:23:01 -0700

    seventh task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,9 +4,10 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
- int factorial(int n);
- int _pow_recursion(int x, int y);
- int _sqrt_recursion(int n);
- int is_prime_number(int n);
+int prime(int a, int j);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 int _sqrt_recurses(int n, int i);
 #endif
