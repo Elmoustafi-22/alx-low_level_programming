b9521a6d38e3ef7c977a6a9ce44b7f5563b6950e
mustopha <mustophaqadir@gmail.com>
2022-10-05 15:47:54 -0700

    third task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - length of a string
- * @s: input char
- * Return: length of a string
- */
-
-int _strlen(char *s)
-{
-	int l = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		l++;
-	}
-	return (l);
-}
-
-/**
   *str_concat - change value using
   *@s1: manipulates char1
   *@s2: manipulates char2
@@ -27,24 +9,39 @@ int _strlen(char *s)
   */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int a, b;
-	char *p, *tmp;
+	char *s3;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (!s1)
-		s1 = "";
-	else
-		a = _strlen(s1);
-	if (!s2)
-		s2 = "";
-	else
-		b = _strlen(s2);
-	p = malloc(a + b + 1);
-	if (!p)
-		return (0);
-	tmp = p;
-	while (*s1)
-		*tmp++ = *s1++;
-	while ((*tmp++ = *s2++))
-		;
-	return (p);
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s3 == NULL)
+		return (NULL);
+
+	i = 0;
+	j = 0;
+
+	if (s1)
+	{
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
+	}
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
+
+	return (s3);
 }
