7b885862888eddf4457342ee58f7669daa91150b
mustopha <mustophaqadir@gmail.com>
2022-10-27 14:53:47 -0700

    fifth

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets index to 0
+ * @n: contains decimal
+ * @index: containing the index.
+ *
+ * Return: converted number
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
