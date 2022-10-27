a123ff8dd30db3801dcbefc192c1557f6961e6cd
mustopha <mustophaqadir@gmail.com>
2022-10-27 15:05:22 -0700

    sixth

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits to flip.
+ * @n: containing the number
+ * @m: containing the number
+ * Return: the converted number
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return (countSetBits(n ^ m));
+}
+
+/**
+ * countsSetBits - counter
+ * @n: containing the number
+ * Return: the converted number
+ */
+int countsSetBits(unsigned long int n)
+{
+	unsigned long int count = 0;
+
+	while (n > 0)
+	{
+		count++;
+		n &= (n - 1);
+	}
+	if (count > sizeof(n) * 8)
+		return (-1);
+	return (count);
+}
